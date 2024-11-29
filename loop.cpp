9d2535d310c39efec963b2a0843fc7758ad0bb5b
MercylineKemunto <mercylinekemunto580@gmail.com>
2024-11-29 22:49:03 +0300

    stuff

---
+++ loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    // Print numbers from 1 to 5
+    while (i <= 10) {
+        cout << i << " ";
+        i++;  // Update loop control variable
+    }
+    return 0;
+}
